47678e8f92c98be07bd5c4b6afe404aaacd11c46
abdulaziz <abdulaziz.hikmatov2001@gmail.coim>
2025-01-09 17:40:05 +0500

    commit1

--- src/main/kotlin/com/example/contract_generator/Utils.kt
+++ src/main/kotlin/com/example/contract_generator/Utils.kt
@@ -2,10 +2,10 @@ package com.example.contract_generator
 
 import org.springframework.security.core.context.SecurityContextHolder
 
-fun getCurrentUserId(): Long? {
+fun getCurrentUserId(): User? {
     val authentication = SecurityContextHolder.getContext().authentication
     if (authentication != null && authentication.principal is User) {
-        return (authentication.principal as User).id
+        return (authentication.principal as User)
     }
     return null
 }
